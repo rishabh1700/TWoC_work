38d928dfbe17fd95f9de2a11a405118fab32f948
Rishabh Srivastava <rishabh1srivastava123@gmail.com>
2020-05-13 01:02:04 +0530

    Add files via upload

---
+++ TWoC 1.py
@@ -0,0 +1,11 @@
+Name = input("Enter your name : ")
+Branch = input("Enter your branch : ")
+Gender = input("Enter your gender : ")
+College = input("Enter your college name : ")
+Age = int(input("Enter your age : "))
+
+print("Name :",Name)
+print("Branch :",Branch)
+print("Gender :",Gender)
+print("College :",College)
+print("Age :",Age)
