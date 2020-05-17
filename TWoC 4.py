dab9f11c815f17594b4567b2073678b46b808922
Rishabh Srivastava <rishabh1srivastava123@gmail.com>
2020-05-18 00:26:14 +0530

    Add files via upload

---
+++ TWoC 4.py
@@ -0,0 +1,8 @@
+# Swap two variable without using a third variable
+
+x = input("Enter first variable : ")
+y = input("Enter second variable : ")
+
+print("Before swapping - " ,x, "",y)
+x,y=y,x
+print("After swapping - ", x,"",y)
